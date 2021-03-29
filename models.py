1a5a3cf6ab50d2ada58c776b6b7c15271b1bd5b3
rgoshen <rick.goshen@gmail.com>
2021-03-29 12:39:15 -0700

    added serialization

--- models.py
+++ models.py
@@ -18,6 +18,17 @@ class Cupcake(db.Model):
     rating = db.Column(db.Float, nullable=False)
     image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
 
+    def serialize(self):
+        """Serialize cupcake to a dict of cupcake info."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "rating": self.rating,
+            "size": self.size,
+            "image": self.image,
+        }
+
 
 def connect_db(app):
     """Connect to database."""
