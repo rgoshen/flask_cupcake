9602e415520afcbaffb7df5686febecb3fc75257
rgoshen <rick.goshen@gmail.com>
2021-03-29 11:44:50 -0700

    added conneciton to db

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 DEFAULT_IMAGE = "https://tinyurl.com/demo-cupcake"
 
+
 class Cupcake(db.Model):
     """Cupcake."""
 
@@ -16,3 +17,10 @@ class Cupcake(db.Model):
     size = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Float, nullable=False)
     image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
